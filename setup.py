c76a172a474718156cae53ece7a6163eb4bb1590
Jason Pearson <jason.d.pearson@gmail.com>
2016-06-30 20:30:36 -0400

    ensure requirements is respected on pip install

--- setup.py
+++ setup.py
@@ -1,7 +1,14 @@
-from distutils.core import setup
+import os
+from setuptools import setup, find_packages
+from pip.req import parse_requirements
 
-setup(name='traildb',
-      version='0.0.1',
-      description='TrailDB stores and queries cookie trails from raw logs.',
-      author='AdRoll.com',
-      packages=['traildb'])
+requirements_path = os.sep.join([os.path.dirname(os.path.abspath(__file__)), 'requirements.txt'])
+install_reqs = parse_requirements(requirements_path, session=False)
+
+setup(
+    name='traildb',
+    version='0.0.1',
+    description='TrailDB stores and queries cookie trails from raw logs.',
+    author='AdRoll.com',
+    packages=['traildb'],
+    install_requires=install_reqs)
