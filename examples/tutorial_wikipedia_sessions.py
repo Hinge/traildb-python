0e2d640d543461d77d375c5dd3985012893cb50f
Jason Pearson <jason.d.pearson@gmail.com>
2016-07-05 17:14:15 -0400

    updated examples

--- examples/tutorial_wikipedia_sessions.py
+++ examples/tutorial_wikipedia_sessions.py
@@ -3,6 +3,7 @@ from traildb import TrailDB
 
 SESSION_LIMIT = 30 * 60
 
+
 def sessions(tdb):
     for i, (uuid, trail) in enumerate(tdb.trails(only_timestamp=True)):
         prev_time = trail.next()
@@ -13,11 +14,10 @@ def sessions(tdb):
                 num_sessions += 1
             prev_time = timestamp
             num_events += 1
-        print 'Trail[%d] Number of Sessions: %d Number of Events: %d' %\
-              (i, num_sessions, num_events)
+        print('Trail[{}] Number of Sessions: {} Number of Events: {}'.format(i, num_sessions, num_events))
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
-        print 'Usage: tutorial_wikipedia_sessions <wikipedia-history.tdb>'
+        print('Usage: tutorial_wikipedia_sessions <wikipedia-history.tdb>')
     else:
         sessions(TrailDB(sys.argv[1]))
