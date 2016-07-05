580b8621ebfd93a0259e76f86cb6221d8061e34d
Jason Pearson <jason.d.pearson@gmail.com>
2016-07-05 17:32:02 -0400

    add debug printing

--- setup.py
+++ setup.py
@@ -6,11 +6,12 @@ requirements_path = os.sep.join([os.path.dirname(os.path.abspath(__file__)), 're
 install_reqs = parse_requirements(requirements_path, session=False)
 
 reqs = [str(req) for req in install_reqs]
-
+print(install_reqs)
+print(reqs)
 setup(
     name='traildb',
     version='0.0.1',
     description='TrailDB stores and queries cookie trails from raw logs.',
     author='AdRoll.com',
-    packages=['traildb'],
+    packages=find_packages(),
     install_requires=reqs)
