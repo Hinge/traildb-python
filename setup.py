deaca0cc40c989c69af40dc4471e60e1fff504bf
Jason Pearson <jason.d.pearson@gmail.com>
2016-07-05 17:35:25 -0400

    make it easier to get requirements

--- setup.py
+++ setup.py
@@ -1,17 +1,12 @@
-import os
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
 
-requirements_path = os.sep.join([os.path.dirname(os.path.abspath(__file__)), 'requirements.txt'])
-install_reqs = parse_requirements(requirements_path, session=False)
-
-reqs = [str(req) for req in install_reqs]
-print(install_reqs)
-print(reqs)
 setup(
     name='traildb',
     version='0.0.1',
     description='TrailDB stores and queries cookie trails from raw logs.',
     author='AdRoll.com',
     packages=find_packages(),
-    install_requires=reqs)
+    install_requires=[
+        'future==0.15.2',
+        'nose2==0.5.0'
+    ])
