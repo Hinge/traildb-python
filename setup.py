343b53cb1b0ff52906669214db3afdd90d2ab35b
Jason Pearson <jason.d.pearson@gmail.com>
2016-07-05 17:30:05 -0400

    stringify requirements

--- setup.py
+++ setup.py
@@ -5,10 +5,12 @@ from pip.req import parse_requirements
 requirements_path = os.sep.join([os.path.dirname(os.path.abspath(__file__)), 'requirements.txt'])
 install_reqs = parse_requirements(requirements_path, session=False)
 
+reqs = [str(req) for req in install_reqs]
+
 setup(
     name='traildb',
     version='0.0.1',
     description='TrailDB stores and queries cookie trails from raw logs.',
     author='AdRoll.com',
     packages=['traildb'],
-    install_requires=install_reqs)
+    install_requires=reqs)
